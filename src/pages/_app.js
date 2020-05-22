3208f043b6c957b4c76f7cba02e57513d196156c
Kartik Nair <kartiknair1911@gmail.com>
2020-05-22 16:03:30 +0400

    Page transitions

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,31 @@
 import "public/styles/base.css";
 import "public/styles/hljs.css";
 import Nav from "src/components/Nav";
+import { PageTransition } from "next-page-transitions";
 
 function MyApp({ Component, pageProps }) {
   return (
     <main>
       <Nav />
-      <Component {...pageProps} />
+      <PageTransition timeout={300} classNames="transition">
+        <Component {...pageProps} />
+      </PageTransition>
+      <style jsx global>{`
+        .transition-enter {
+          opacity: 0;
+        }
+        .transition-enter-active {
+          opacity: 1;
+          transition: opacity 300ms;
+        }
+        .transition-exit {
+          opacity: 1;
+        }
+        .transition-exit-active {
+          opacity: 0;
+          transition: opacity 300ms;
+        }
+      `}</style>
     </main>
   );
 }
